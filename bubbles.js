0ea8db6da2d7fafbedad81dd3e4db592f5000750
Prateena <34796312+Prateena@users.noreply.github.com>
2018-07-12 13:21:03 -0400

    Add files via upload

--- bubbles.js
+++ bubbles.js
@@ -25,33 +25,33 @@ var svg = d3.select("#chart")
             .await(ready)
 
             function ready(error, datapoints) {
+            
+            	var circles = svg.selectAll(".artist")
+            	.data(datapoints)
+            	.enter().append("circle")
+            	.attr("class", "artist")
+            	.attr("r", function(d)
+            	{
+            		return radiusScale(d.sales);
+            	})
+    
+            	.attr("fill", "Purple")
 
-              var node = svg.selectAll(".node")
-    .data(pack(root).leaves())
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-  node.append("circle")
-      .attr("id", function(d) { return d.id; })
-      .attr("r", function(d) { return d.r; })
-      .style("fill", function(d) { return d.data.fill; });
 
-  node.append("clipPath")
-      .attr("id", function(d) { return "clip-" + d.id; })
-    .append("use")
-      .attr("xlink:href", function(d) { return "#" + d.id; });
+            
 
-  node.append("text")
-      .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
-    .selectAll("tspan")
-    .data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
-    .enter().append("tspan")
-      .attr("x", 0)
-      .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-      .text(function(d) { return d; });
-
-  node.append("title")
-      .text(function(d) { return d.id + "\n" + format(d.value); });
+    simulation.nodes(datapoints)
+              .on('tick', ticked)
+    
+    function ticked() {
+    	circles
+    	.attr("cx",function(d) {
+    		return d.x
+    	})
+        .attr("cy", function(d) {
+        	return d.y
+        })
+    }        
             }
 })();
